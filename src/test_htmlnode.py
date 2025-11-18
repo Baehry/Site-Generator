3163c621ae9b532e530738c50e6a26c291cae0a5
Baehry <asaf2006b@gmail.com>
2025-11-18 18:58:52 +0200

    5: added src/htmlnode.py and src/test_htmlnode.py

---
+++ src/test_htmlnode.py
@@ -0,0 +1,9 @@
+import unittest
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props_to_html(self):
+        self.assertEqual(0, 0)
+
+if __name__ == "__main__":
+    unittest.main()

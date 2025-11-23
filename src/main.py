4fce8d59ea7c56d700171a7641d07406e817ec71
Baehry <asaf2006b@gmail.com>
2025-11-23 17:23:45 +0200

    14: added static dir and copy_files function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,23 @@
-
 from functions import *
+import os
+import shutil
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(node)
+    copy_files(os.getcwd(), "static", "public")
+
+def copy_files(working_directory, source_directory, destination_directory):
+    source_path = os.path.join(working_directory, source_directory)
+    dest_path = os.path.join(working_directory, destination_directory)
+    if not (source_path.startswith(working_directory) and dest_path.startswith(working_directory)):
+        raise Exception("illegal directory")
+    if os.path.exists(dest_path):
+        shutil.rmtree(dest_path)
+    if os.path.isfile(source_path):
+        shutil.copy(source_path, dest_path)
+        return
+    os.mkdir(dest_path)
+    files = os.listdir(source_path)
+    for file in files:
+        copy_files(working_directory, os.path.join(source_directory, file), os.path.join(destination_directory, file))
 
 main()
